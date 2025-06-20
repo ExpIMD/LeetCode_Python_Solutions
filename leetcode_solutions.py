fbb576b25bdbd88d578919804c2cdc18c89ae3b2
ExpIMD <fourexpimd@gmail.com>
2025-06-20 10:19:46 +0300

    Comment fixes and solving the <35. Search Insert Position> Problem

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -1,8 +1,9 @@
 def two_sum(numbers: list[int], target: int) -> tuple[int, int]:
     """
-    Given a list of integers %numbers% and an integer %target%
-    Returns a tuple of two indices of elements in a vector whose sum equals the %target%
-    If there is no solution, then returns a tuple of two zeros
+    Description:
+        Given a list of integers %numbers% and an integer %target%
+        Returns a tuple of two indices of elements in a list whose sum equals the %target%
+        If there is no solution, then returns a tuple of two zeros
     """
 
     indices_elements: list[tuple[int, int]] = list(enumerate(numbers)) # # Create a list of tuples containing original indices and their corresponding numbers
@@ -22,3 +23,33 @@ def two_sum(numbers: list[int], target: int) -> tuple[int, int]:
         else: # If the sum equals the target, return the original indices of these elements
             return (indices_elements[left][0], indices_elements[right][0])
     return (0, 0) # No solution
+
+def search_insert_position(lst: list[object], target: object) -> int:
+    """
+    Description:
+        Given a sorted list %lst% and a value %target%
+        Returns the index of %target% in %lst% if it exists
+        Otherwise, returns the index where <target> would theoretically be inserted to maintain sorted order
+
+    Note:
+        The objects in %lst% and %target% must support the '<' operator.
+    """
+    left: int = 0
+    right: int = len(lst)
+    # left and right are pointers to elements in the vector on both sides
+
+    while left < right: # We will consider the range [left; right)
+        # Using an open interval is not necessary, since Python integers do not overflow
+        # We can safely use either while 'left < right' with 'right = middle' or while 'left <= right' with 'right = middle - 1'
+
+        middle: int = (left + right) // 2
+
+        # For convenience, only the operator<() will be used
+		# We shift the left and right pointers
+        if lst[middle] < target:
+            left = middle + 1
+        elif target < lst[middle]:
+            right = middle
+        else:
+            return middle # Found target
+    return left # Returns the index in the list where the target would theoretically be positioned 
